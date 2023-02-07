8ec13c4322ccdb7b218d016eda754c6985f034df
DaduPark <koreatkdtn@gmail.com>
2023-02-07 15:03:25 +0900

    알파벳 개수

--- src/section1_String/Test1.java
+++ src/section1_String/Test1.java
@@ -1,10 +1,33 @@
 package section1_String;
 
+import java.util.Scanner;
+
 public class Test1 {
 
+	public int solution(String str, char c) {
+		
+		str = str.toUpperCase();
+		c = Character.toUpperCase(c);
+		
+		int n = 0;
+		
+		for(char strOne : str.toCharArray()) {
+			if(c == strOne) {
+				n++;
+			}
+		}
+		
+		
+		return n;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
- 
+		Test1 main = new Test1();
+		
+		Scanner in=new Scanner(System.in);
+		String str = in.nextLine();
+		char c = in.next().charAt(0);
+		
+		System.out.println(main.solution(str, c));
 	}
 
 }
