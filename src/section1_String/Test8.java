4a84b2c642ac884c6171f1ab11316c46d9a10685
DaduPark <koreatkdtn@gmail.com>
2023-02-24 21:25:15 +0900

    팰린드롬(앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열)

---
+++ src/section1_String/Test8.java
@@ -0,0 +1,61 @@
+package section1_String;
+
+import java.util.Scanner;
+
+public class Test8 {
+	
+	//팰린드롬(앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열) 
+	public String solution(String str) {
+	   
+	  String alphabetStr = getAlphabetStr(str);
+	  int arrayLastNum = alphabetStr.length()-1;
+	  
+	  char[] strArray = alphabetStr.toCharArray();
+	  
+	  for(char strOne : strArray) {
+	   if(Character.toUpperCase(strOne) != Character.toUpperCase(strArray[arrayLastNum--])) {
+	    return "NO";
+	   }
+	  }
+	  
+	  return "YES";
+	}
+	
+	public String getAlphabetStr(String str) {
+		StringBuffer alphabetStr = new StringBuffer();
+		
+		char[] strArray = str.toCharArray();
+		for(char strOne : strArray) {
+			if(isAlphabet(strOne)) {
+				alphabetStr.append(strOne);
+			}
+		}
+		
+		return alphabetStr.toString();
+	}
+	
+	private boolean isAlphabet(char oneStr) {
+		
+		if(oneStr >= 65 && oneStr <= 90) {
+			return true;
+		}
+		
+		if(oneStr >= 97 && oneStr <= 122) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	
+	
+	
+	public static void main(String[] args) {
+		Test8 main = new Test8();
+		
+		Scanner in=new Scanner(System.in);
+		
+		System.out.println(main.solution(in.nextLine()));
+	}
+
+}
