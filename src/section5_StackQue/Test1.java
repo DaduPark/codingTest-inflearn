459d269a5f0cb4c75608c7da45dca17732a46af8
pss3420 <pss3420@shinsegae.com>
2023-04-03 12:35:24 +0900

    Stack  1문제

---
+++ src/section5_StackQue/Test1.java
@@ -0,0 +1,60 @@
+package section5_StackQue;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Test1 {
+	public String  solution(String str ) {
+		Stack<Character> stack = new Stack<>();
+		
+		for(char strOne : str.toCharArray()) {
+			
+			if(strOne!=')') {
+				stack.push(strOne);
+			}else {
+				stackPop(stack);
+			}
+		}
+		
+		return stackToStr(stack);
+	}
+	void stackPop(Stack<Character> stack ) {
+		
+		while(true) {
+			
+			if(stack.peek()=='(') {
+				stack.pop();
+				break;
+			}
+			
+			stack.pop();
+		}
+		
+	}
+	
+	public String stackToStr(Stack<Character> stack) {
+		
+		StringBuilder str = new StringBuilder();
+		while(!stack.isEmpty()) {
+			str.append(stack.pop());
+		}
+
+		return str.reverse().toString();
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		Test1 main = new Test1();
+		
+		Scanner in=new Scanner(System.in);
+		
+		String str = in.nextLine();
+		
+		
+		System.out.println(main.solution(str));
+		
+	}
+
+}
